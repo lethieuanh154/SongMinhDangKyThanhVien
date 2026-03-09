66c7b380367476077a108736834d186c516d351e
lethieuanh89 <leanh1541989@gmail.com>
2026-03-09 21:18:28 +0700

    remove first

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
-import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { CompleteProfilePageComponent } from './pages/complete-profile-page/complete-profile-page.component';
 import { CardPageComponent } from './pages/card-page/card-page.component';
 
 export const routes: Routes = [
-  { path: '', component: LoginPageComponent },
-  { path: 'login', component: LoginPageComponent },
+  { path: '', component: CompleteProfilePageComponent },
   { path: 'complete-profile', component: CompleteProfilePageComponent },
   { path: 'card/:customer_code', component: CardPageComponent },
   { path: '**', redirectTo: '/' },
